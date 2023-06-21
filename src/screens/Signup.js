459eefa02300396de0a03c6eb765902d57b4cef3
Tejaswini306 <saitejaswinij2002@gmail.com>
2023-06-22 01:48:47 +0530

    frontend updated

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -7,7 +7,7 @@ export default function Signup() {
   let navigate = useNavigate()
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/api/auth/createuser", {
+    const response = await fetch("http://13.49.66.70:5000/api/auth/createuser", {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
